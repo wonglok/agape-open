6fc9e62d25db427bee310165af866e85aa22a083
Wong Lok <wonglok@users.noreply.github.com>
2022-07-08 16:00:29 +0800

    yo

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Invoices } from "./pages/Invoices";
-import { Expenses } from "./pages/Expenses";
 import { LandingPage } from "./pages/Landing/LandingPage";
+import { About } from "./pages/About/About";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/expenses" element={<Expenses />} />
-        <Route path="/invoices" element={<Invoices />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
